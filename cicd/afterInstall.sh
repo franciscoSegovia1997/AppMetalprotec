efdcb0444e551d1ad019200fb606fc559ad0943a
Alexander Segovia <xander2497@gmail.com>
2023-10-04 10:14:43 -0500

    [ADD] Deployment fixed

--- cicd/afterInstall.sh
+++ cicd/afterInstall.sh
@@ -5,8 +5,10 @@ sudo chown -cR ubuntu:ubuntu /var/{log,run}/gunicorn/
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/AppMetalprotec/requirements.txt
+sudo pkill -9 gunicorn
 cd /home/ubuntu/AppMetalprotec/
-gunicorn -c config/gunicorn/dev.py
+gunicorn -c config/gunicorn/dev.py &
+sleep 5
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
 sudo systemctl restart nginx
