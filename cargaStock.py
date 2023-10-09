00c8c36bd20041900548eaf54ec489da2a194277
Alexander Segovia <xander2497@gmail.com>
2023-10-09 06:31:02 -0500

    [ADD] Stock updated for chimbote

--- cargaStock.py
+++ cargaStock.py
@@ -11,35 +11,43 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AppMetalprotec.settings")
 django.setup()
 
 from productsMetalprotec.models import productSystem, storeSystem, storexproductSystem
+from django.contrib.auth.models import User
+from usersMetalprotec.models import extendedUser
 
-filename = 'infoStock.csv'
+usuarioRaiz = User.objects.get(username='adminMetalprotec2023')
+endpointProduct = usuarioRaiz.extendeduser.endpointUser
+
+filename = 'newStock.csv'
 
 with open(filename, 'r') as csv_file:
     reader = csv.reader(csv_file)
     for row in reader:
         print(f"Codigo a inspeccionar : {row[0]}")
         try:
-            infoProducto = productSystem.objects.get(codeProduct=row[0])
+            infoProducto = productSystem.objects.filter(endpointProduct=endpointProduct).get(codeProduct=row[0])
             print(f"CODIGO UNICO - {row[0]} CON STOCK {row[1]}")
         except:
             print(f"CODIGO REPETIDO!! - {row[0]}")
 
 #SEGUNDA ETAPA : ELIMINAR EL STOCK ACTUAL
 
-#totalStock = storexproductSystem.objects.all()
-#for stockInfo in totalStock:
-#    print(f"MODIFICANDO EL STOCK DEL PRODUCTO: {stockInfo.asociatedProduct.nameProduct} - EN EL ALMACEN: {stockInfo.asociatedStore.nameStore}")
-#    stockInfo.quantityProduct = '0.00'
-#    stockInfo.save()
-#    print("STOCK MODIFICADO")
+totalStock = storexproductSystem.objects.all().filter(asociatedStore__nameStore='CHIMBOTE')
+for stockInfo in totalStock:
+    if stockInfo.asociatedStore.nameStore == 'CHIMBOTE':
+        print(f"MODIFICANDO EL STOCK DEL PRODUCTO: {stockInfo.asociatedProduct.nameProduct} - EN EL ALMACEN: {stockInfo.asociatedStore.nameStore}")
+        stockInfo.quantityProduct = '0.00'
+        stockInfo.save()
+        print("STOCK MODIFICADO")
 
 #ACTUALIZAR STOCK
+
+
 with open(filename, 'r') as csv_file:
     reader = csv.reader(csv_file)
     for row in reader:
         print(f"Codigo a inspeccionar : {row[0]}")
         try:
-            infoProducto = productSystem.objects.get(codeProduct=row[0])
+            infoProducto = productSystem.objects.filter(endpointProduct=endpointProduct).get(codeProduct=row[0])
             print(f"CODIGO UNICO - {row[0]} CON STOCK {row[1]}.00")
             objProducto = infoProducto.storexproductsystem_set.all().get(asociatedStore__nameStore='CHIMBOTE')
             objProducto.quantityProduct = f"{row[1]}.00"
